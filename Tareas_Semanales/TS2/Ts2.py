6d915042ada0ae90355c572f986bc9ad963e6ee0
dlugano32 <dlugano@frba.utn.edu.ar>
2023-04-15 21:38:06 -0300

    TS2 v1.1

--- Tareas_Semanales/TS2/Ts2.py
+++ Tareas_Semanales/TS2/Ts2.py
@@ -15,20 +15,33 @@ from pytc2.sistemas_lineales import pzmap, GroupDelay, bodePlot
 
 #Red normalizada
 R1=1/10
-R2=3
+R2=[3, 1/np.sqrt(2)]
 R3=1 #Norma de impedancia
 C=1
 
 w0=1/C*R3 #Frecuencia normalizada -> Norma de frec wo=1/(C*R3)
-Q=C*R2
+Q=[C*R2[0], C*R2[1]]
 
 K=1/((C**2)*R1*R3)
 
 plt.close('all')
-    
-Tf=TransferFunction([K], [1, w0/Q, w0**2])
-bodePlot(Tf, fig_id=1 )
-pzmap(Tf, fig_id=2 )
-GroupDelay(Tf, fig_id=3)
+
+#Filtro Pasabajo generico con Q=3 y w0=1
+TFPB=TransferFunction([K], [1, w0/Q[0], w0**2])
+bodePlot(TFPB, fig_id=1)
+pzmap(TFPB, fig_id=2 )
+GroupDelay(TFPB, fig_id=3)
+
+#Filtro Butterworth de segundo orden
+TFPBbut=TransferFunction([K], [1, w0/Q[1], w0**2])
+bodePlot(TFPBbut, fig_id=5)
+pzmap(TFPBbut, fig_id=6 )
+GroupDelay(TFPBbut, fig_id=7)
+
+#Filtro Pasabanda
+TFPBand=TransferFunction([1/(C*R1), 0], [1, w0/Q[0], w0**2])
+bodePlot(TFPBand, fig_id=8 )
+pzmap(TFPBand, fig_id=9 )
+GroupDelay(TFPBand, fig_id=10 )
 
 
