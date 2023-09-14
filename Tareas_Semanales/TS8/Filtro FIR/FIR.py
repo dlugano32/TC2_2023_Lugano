73da0208ebfe3f5587eb6508aba55c7d499f1144
dlugano <dlugano@frba.utn.edu.ar>
2023-09-14 15:21:11 -0300

    TS8

---
+++ Tareas_Semanales/TS8/Filtro FIR/FIR.py
@@ -0,0 +1,37 @@
+# Inicialización e importación de módulos
+
+# Módulos para Jupyter
+import warnings
+warnings.filterwarnings('ignore')
+
+# Módulos importantantes
+import scipy.signal as sig
+import numpy as np
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import scipy.io as sio
+from pytc2.sistemas_lineales import plot_plantilla
+
+fig_sz_x = 10
+fig_sz_y = 7
+fig_dpi = 100 # dpi
+
+fig_font_size = 16
+
+mpl.rcParams['figure.figsize'] = (fig_sz_x,fig_sz_y)
+plt.rcParams.update({'font.size':fig_font_size})
+
+###
+## Señal de ECG registrada a 1 kHz, con contaminación de diversos orígenes.
+###
+
+# para listar las variables que hay en el archivo
+#io.whosmat('ecg.mat')
+mat_struct = sio.loadmat('ecg.mat')
+
+ecg_one_lead = mat_struct['ecg_lead']
+ecg_one_lead = ecg_one_lead.flatten()
+cant_muestras = len(ecg_one_lead)
+
+fs = 1000 # Hz
+nyq_frec = fs / 2
